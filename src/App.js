9c7ed80ea7999878fed34d428b514fc8e6a5afd9
Suraj <surajbasnet449@gmail.com>
2023-02-27 14:24:57 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("#000000");
+
+  const generateColor = () => {
+    const hexValues = "0123456789ABCDEF";
+    let newColor = "#";
+    for (let i = 0; i < 6; i++) {
+      newColor += hexValues[Math.floor(Math.random() * 16)];
+    }
+    setColor(newColor);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HexaDecimal Color Generator</h1>
+      <button onClick={generateColor}>{color}</button>
     </div>
   );
 }
---
+++ src/Mycomponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App";
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>My Component</h1>
+      <App />
+    </div>
+  );
+};
+export default MyComponent;
